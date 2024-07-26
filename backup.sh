2b84180b2d622d535e210fd8494d92a197b9e20c
malen <malen.ma@gmail.com>
2024-07-26 17:50:53 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,5 +7,5 @@ bakup_name=$1.tar.gz
 # 上传文件到box的根目录(box官网的curl命令中的url是错误的，缺少api路径)
 curl -i -X POST "https://upload.box.com/api/2.0/files/content" \
      -H "authorization: Bearer $accessToken" \
-     -F attributes='{"name":"'${bakup_name}', "parent":{"id":"0"}}' \
+     -F attributes='{"name":"'${bakup_name}'", "parent":{"id":"0"}}' \
      -F file=@$1.tar.gz
