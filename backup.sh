275dd6eb9c400bfce8c707efd3088ee01db9e086
malen <malen.ma@gmail.com>
2024-07-26 17:08:29 +0900

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# $1 为需要备份的目录
+tar -czvf backup.tar.gz $1
+
+# 上传文件到box的根目录(box官网的curl命令中的url是错误的，缺少api路径)
+curl -i -X POST "https://upload.box.com/api/2.0/files/content" \
+     -H "authorization: Bearer $accessToken" \
+     -F attributes='{"name":"backup.tar.gz", "parent":{"id":"0"}}' \
+     -F file=@backup.tar.gz
