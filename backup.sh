f656630d1863c4f2780aade28bc21bc507297068
malen <malen.ma@gmail.com>
2024-07-26 18:46:26 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,17 +3,33 @@
 if [[ "$1" == "-h" ]];then
     echo "Usage: ./backup.sh <folder_path> [<backup_name>]"
 else
-    # $1 为需要备份的目录(log文件除外)
-    tar --exclude='*.log' -czvf $2.tar.gz $1
-    
     if [[ -z $2 ]];then
-        bakup_name=backup.tar.gz
+        bakup_file=backup.tar.gz
     else
-        bakup_name=$2.tar.gz
+        bakup_file=$2.tar.gz
     fi
-    # 上传文件到box的根目录(box官网的curl命令中的url是错误的，缺少api路径)
-    curl -i -X POST "https://upload.box.com/api/2.0/files/content" \
-         -H "authorization: Bearer $accessToken" \
-         -F attributes='{"name":"'${bakup_name}'", "parent":{"id":"0"}}' \
-         -F file=@$2.tar.gz
+    # $1 为需要备份的目录(log文件除外)
+    tar --exclude='*.log' -czvf $bakup_file $1
+
+    # 判断文件是否超过box的250M限制
+    MAX_SIZE=$((250 * 1024 * 1024))
+    FILE_SIZE=$(stat -c%s "$bakup_file")
+    if [[ "$FILE_SIZE" > "$MAX_SIZE" ]]; then
+        # 分割文件
+        split -b 250M "$bakup_file" part$2_
+        # 上传每一个分割文件
+        for file in part$2_*; do
+            curl -i -X POST "https://upload.box.com/api/2.0/files/content" \
+                 -H "authorization: Bearer $accessToken" \
+                 -F attributes='{"name":"'${file}'", "parent":{"id":"0"}}' \
+                 -F file=@$file
+        done
+    else
+    
+        # 上传文件到box的根目录(box官网的curl命令中的url是错误的，缺少api路径)
+        curl -i -X POST "https://upload.box.com/api/2.0/files/content" \
+             -H "authorization: Bearer $accessToken" \
+             -F attributes='{"name":"'${bakup_file}'", "parent":{"id":"0"}}' \
+             -F file=@$bakup_file
+    fi
 fi
